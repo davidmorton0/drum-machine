0e163bf1d5f5f7353e5f16d3c664595260a6d35d
David Morton <55235981+davidmorton0@users.noreply.github.com>
2020-03-05 16:46:21 +0000

    move files

--- src/index.js
+++ src/index.js
@@ -6,47 +6,47 @@ import * as serviceWorker from './serviceWorker';
 var drumSounds = {
   Q: {
     text: 'Q',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+    url: './assets/audio/Brk_Snr.mp3',
     description: 'Snare'
   },
   W: {
     text: 'W',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+    url: './assets/audio/side_stick_1.mp3',
     description: 'Side Stick'
   },
   E: {
     text: 'E',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+    url: './assets/audio/punchy_kick_1.mp3',
     description: 'Punchy Kick'
   },
   A: {
     text: 'A',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+    url: './assets/audio/Bld_H1.mp3',
     description: 'Closed High Hat'
   },
   S: {
     text: 'S',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+    url: './assets/audio/Dry_Ohh.mp3',
     description: 'Open High Hat'
   },
   D: {
     text: 'D',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    url: './assets/audio/Dsc_Oh.mp3',
     description: 'Dsc Oh'
   },
   Z: {
     text: 'Z',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    url: './assets/audio/Kick_n_Hat.mp3',
     description: 'Kick n Hat'
   },
   X: {
     text: 'X',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    url: './assets/audio/RP4_KICK_1.mp3',
     description: 'Kick'
   },
   C: {
     text: 'C',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    url: './assets/audio/Cev_H2.mp3',
     description: 'Cymbal'
   }
 }
