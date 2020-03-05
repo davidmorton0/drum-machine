fd8c8fb4ad0b6f675d0b1a1d47ba270c3cc58d0c
David Morton <davidmorton0@gmail.com>
2020-03-05 15:22:24 +0000

    Updates

---
+++ precache-manifest.fc84ee55b4457dbf7a55c4906e639960.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85acdb2eb709e5f1a433577d98aefc5a",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "11b29cb404ce8ee588e3",
+    "url": "/drum-machine/static/css/main.5470ffc4.chunk.css"
+  },
+  {
+    "revision": "9d45c2180fd98bd8198b",
+    "url": "/drum-machine/static/js/2.a2b31b18.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drum-machine/static/js/2.a2b31b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b29cb404ce8ee588e3",
+    "url": "/drum-machine/static/js/main.e55667cb.chunk.js"
+  },
+  {
+    "revision": "8693eb012ac077547e9e",
+    "url": "/drum-machine/static/js/runtime-main.1b00ec3c.js"
+  },
+  {
+    "revision": "985079a01830cd0d84e5c3b71a9d08dc",
+    "url": "/drum-machine/static/media/bass_drum.985079a0.jpg"
+  },
+  {
+    "revision": "317b6905fd13fb3b0f863d9d5b83e6fd",
+    "url": "/drum-machine/static/media/closed_high_hat.317b6905.jpg"
+  },
+  {
+    "revision": "e96f57a802d29bc7b8e4ed723382af82",
+    "url": "/drum-machine/static/media/cross_stick.e96f57a8.jpg"
+  },
+  {
+    "revision": "4ef4cc8560d11569897997176fa71a1c",
+    "url": "/drum-machine/static/media/cymbal.4ef4cc85.jpg"
+  },
+  {
+    "revision": "d4c351ddd415d0591c1b8c4e41259a22",
+    "url": "/drum-machine/static/media/cymbal_2.d4c351dd.jpg"
+  },
+  {
+    "revision": "b44b2b4d710490cb6c4ae98d2f17284e",
+    "url": "/drum-machine/static/media/kick.b44b2b4d.jpg"
+  },
+  {
+    "revision": "995a5339b987322987bc7da4889880c7",
+    "url": "/drum-machine/static/media/kick_hat.995a5339.jpg"
+  },
+  {
+    "revision": "9ca4854ab3cf97f34d773ea09ec8bb5c",
+    "url": "/drum-machine/static/media/open_high_hat.9ca4854a.jpg"
+  },
+  {
+    "revision": "93a87b8ae305536613ec22a6af3c731e",
+    "url": "/drum-machine/static/media/snare.93a87b8a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drum-machine/precache-manifest.f6d4c516de94bf9382083c8f9b326cb7.js"
+  "/drum-machine/precache-manifest.fc84ee55b4457dbf7a55c4906e639960.js"
 );
 
 self.addEventListener('message', (event) => {
